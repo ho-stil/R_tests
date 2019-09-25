8ba4224ee15274de3e73bf431cdd1bce2e6be8b3
ho-stil <54687396+ho-stil@users.noreply.github.com>
2019-09-25 14:41:31 +0200

    Add files via upload

---
+++ Plotly_Bug_ErrorBarColor_Workaround.R
@@ -0,0 +1,21 @@
+# reproducible excample for plotly-problem:
+
+library(crosstalk)
+library(plotly)
+TestDF <- data.frame(a = c(1:9), b = c(rep(c(1:3), 3)), c = c(1:9/10), d = LETTERS[rep(c(1:3),3)])
+d_list <- unique(TestDF$d)
+shared_Test_all <- SharedData$new(TestDF, group = "Test")
+shared_Test <- vector("list", 3)
+p1 <- plot_ly(type = "scatter", mode = "markers")  
+for (i in 1:length(d_list)) {
+  shared_Test[[i]] <- SharedData$new(TestDF[TestDF$d == d_list[i],], group = "Test")
+  p1 <- add_trace(p1, data = shared_Test[[i]], x = ~a, y = ~b, name = ~d, error_y = ~list(array = c))
+}
+bscols(list(filter_select("d", "filter by d", shared_Test_all, ~d, multiple = TRUE),
+            bscols(plot_ly(data = shared_Test_all, x = ~a, y = ~b, 
+                           type = "scatter", mode = "markers",
+                           error_y = ~list(array = c)),
+                   plot_ly(data = shared_Test_all, x = ~a, y = ~b, name = ~d, 
+                           type = "scatter", mode = "markers",
+                           error_y = ~list(array = c)),
+                   p1)))
